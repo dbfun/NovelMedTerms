6d93d98fc0dc02efe885c8f98f5315f6c5083f2e
Вагун Илья <VagunIV@stud.rosnou.ru>
2026-01-20 22:01:04 +0300

    Тема: CRISPR gene therapy за 20 лет

--- launch.sh
+++ launch.sh
@@ -6,14 +6,22 @@
 #
 ##########################################################################################################
 
-echo "=============================================="
-time python workflow.py 'workflows/samples/Cancer breast calcification за 20 лет/PMC_biomedical-ner-all.yaml'
+# Тема: Cancer breast calcification за 20 лет
+
+#echo "=============================================="
+#time python workflow.py 'workflows/samples/Cancer breast calcification за 20 лет/PMC_biomedical-ner-all.yaml'
+#
+#echo "=============================================="
+#time python workflow.py 'workflows/samples/Cancer breast calcification за 20 лет/PMC_gliner-biomed-bi-large-v1.0.yaml'
+#
+#echo "=============================================="
+#time python workflow.py 'workflows/samples/Cancer breast calcification за 20 лет/PMC_OpenBioNER.yaml'
+#
+#echo "=============================================="
+#time python workflow.py 'workflows/samples/Cancer breast calcification за 20 лет/PMC_pos-based-hybrid.yaml'
+
+# Тема: CRISPR за 20 лет
 
 echo "=============================================="
-time python workflow.py 'workflows/samples/Cancer breast calcification за 20 лет/PMC_gliner-biomed-bi-large-v1.0.yaml'
-
-echo "=============================================="
-time python workflow.py 'workflows/samples/Cancer breast calcification за 20 лет/PMC_OpenBioNER.yaml'
-
-echo "=============================================="
-time python workflow.py 'workflows/samples/Cancer breast calcification за 20 лет/PMC_pos-based-hybrid.yaml'
+time python workflow.py 'workflows/samples/CRISPR gene therapy за 20 лет/PMC_biomedical-ner-all.yaml'
+bash backup.sh 'workflows/CRISPR gene therapy за 20 лет/PMC_biomedical-ner-all'
